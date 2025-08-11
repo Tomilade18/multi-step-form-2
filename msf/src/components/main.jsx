03cfbea4491266b592f54befd02375d649a001bc
Tomilade18 <jboyyumzey@gmail.com>
2025-08-11 12:03:01 +0100

    first commit

---
+++ msf/src/components/main.jsx
@@ -0,0 +1,12 @@
+export default function Form() {
+    return (
+        <div className="main">
+            <div className="header"></div>
+            <div className="middle"></div>
+            <div className="bottom">
+                <button className="prev">Go Back</button>
+                <button className="next"> Next Step</button>
+            </div>
+        </div>
+    )
+}
